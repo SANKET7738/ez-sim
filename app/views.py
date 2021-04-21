4878e1414d7a10edb93f71e6afebe77aa99384d4
SANKET7738 <sanket.mhatre@vit.edu.in>
2021-04-21 20:52:19 +0530

    added diode circuits

--- app/views.py
+++ app/views.py
@@ -3,35 +3,91 @@ from flask import render_template, request, redirect, jsonify
 import numpy as np
 import base64
 from io import BytesIO
-from circuit import operational_amplifier, voltage_divider, current_divider
+from circuit import operational_amplifier, voltage_divider, current_divider, diode_characteristic_curve, half_wave_rectifier, full_wave_rectifier
 
 circuitList = { 
         "1": "Voltage-Divider",
-        "2": "Current-Divider"
+        "2": "Current-Divider",
+        "3": "Diode-Characteristic-Curve",
+        "4": "Half-Wave-Rectifier",
+        "5": "Full-Wave-Rectifier",
     }
 circuitImgList = {
     "1": "https://pyspice.fabrice-salvaire.fr/releases/v1.4/_images/voltage-divider.png",
     "2": "https://pyspice.fabrice-salvaire.fr/releases/v1.4/_images/current-divider.png",
+    "3": "https://pyspice.fabrice-salvaire.fr/releases/v1.4/_images/diode-characteristic-curve-circuit.png",
+    "4": "https://pyspice.fabrice-salvaire.fr/releases/v1.4/_images/half-wave-rectification.png",
+    "5": "https://pyspice.fabrice-salvaire.fr/releases/v1.4/_images/full-wave-rectification.png",
 }
 
-args = {}
-args['list'] = circuitList
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    args = {}
+    args['list'] = circuitList
     if request.args:
         raw_req = request.args
         item = raw_req['item']
         if item == "Voltage-Divider":
+            args = {}
+            args['list'] = circuitList
             imgUrl = circuitImgList.get("1")
             args['title'] = item
             args['imgUrl'] = imgUrl
+            inputs = ['Vin', 'R1', 'R2']
+            args['inputs'] = inputs
+            msg = "Units: Vin = V , R1 & R2 = kOhm"
+            args['msg'] = msg
             return render_template("circuit.html", args=args)
+
         if item == "Current-Divider":
+            args = {}
+            args['list'] = circuitList
             imgUrl = circuitImgList.get("2")
             args['title'] = item
             args['imgUrl'] = imgUrl
+            inputs = ['Iin', 'R1', 'R2']
+            args['inputs'] = inputs
+            msg = "Units: Iin = A ,  R1 & R2 = kOhm"
+            args['msg'] = msg
             return render_template("circuit.html", args=args)
+            
+        if item == "Diode-Characteristic-Curve":
+            args = {}
+            args['list'] = circuitList
+            imgUrl = circuitImgList.get("3")
+            args['title'] = item
+            args['imgUrl'] = imgUrl
+            args['inputs'] = ['Vin', 'R']
+            msg = "Units: Vin = V ,  R = Ohm , Diode = '1N4148' "
+            args['msg'] = msg
+            return render_template("circuit.html", args=args)
+        
+        if item == "Half-Wave-Rectifier":
+            args = {}
+            args['list'] = circuitList
+            imgUrl = circuitImgList.get("4")
+            args['title'] = item
+            args['imgUrl'] = imgUrl
+            inputs = ['Vin', 'R' , 'C', 'F']
+            args['inputs'] = inputs
+            msg = "Units: Vin = V ,  R = Ohm , C = mF , F = Hz, Diode = '1N4148' "
+            args['msg'] = msg
+            return render_template("circuit.html", args=args)
+        
+        if item == "Full-Wave-Rectifier":
+            args = {}
+            args['list'] = circuitList
+            imgUrl = circuitImgList.get("5")
+            args['title'] = item
+            args['imgUrl'] = imgUrl
+            inputs = ['Vin', 'R' , 'C', 'F']
+            args['inputs'] = inputs
+            msg = "Units: Vin = V ,  R = Ohm , C = mF , F = Hz, Diode = '1N4148' "
+            args['msg'] = msg
+            return render_template("circuit.html", args=args)
+
   
     return render_template("index.html", args=args)
 
@@ -39,17 +95,72 @@ def index():
 def output():
     print('out')
     formData = request.form
+    args = {}
+    args['list'] = circuitList
     if formData['title'] ==  "Voltage-Divider":
+        args = {}
+        args['list'] = circuitList
+        args['imgUrl'] = formData['imgUrl']
         circuit, analysis, output = voltage_divider(formData['Vin'],formData['R1'],formData['R2'])
         args['title'] = circuit.title
         args['output'] = output
+        inputs = str('Inputs: Vin = {} V , R1 = {} kOhm , R2 = {} kOhm'.format(formData['Vin'], formData['R1'], formData['R2']))
+        args['inputs'] = inputs
         return render_template("output.html", args=args)   
 
     if formData['title'] == "Current-Divider":
-        circuit, analysis, output = current_divider(formData['Vin'],formData['R1'],formData['R2'])
+        args = {}
+        args['list'] = circuitList
+        args['imgUrl'] = formData['imgUrl']
+        circuit, analysis, output = current_divider(formData['Iin'],formData['R1'],formData['R2'])
         args['title'] = circuit.title
-        args['output'] = output
-        return render_template("output.html", args=args)    
+        args['output'] = output 
+        inputs = str('Inputs: Iin = {} V , R1 = {} kOhm , R2 = {} kOhm'.format(formData['Iin'], formData['R1'], formData['R2']))
+        args['inputs'] = inputs
+        return render_template("output.html", args=args)   
+
+    if formData['title'] == "Diode-Characteristic-Curve":
+        args = {}
+        args['list'] = circuitList
+        args['imgUrl'] = formData['imgUrl']
+        circuit, analysis, plot = diode_characteristic_curve(formData['Vin'], formData['R'])
+        args['title'] = circuit.title
+        buf = BytesIO()
+        plot.savefig(buf, format="png")
+        data = base64.b64encode(buf.getbuffer()).decode("ascii")
+        args['plot'] = data
+        inputs = str('Inputs: Vin = {} V , R = {} Ohm'.format(formData['Vin'], formData['R']))
+        args['inputs'] = inputs
+        return render_template("output.html", args=args)
+    
+    if formData['title'] == "Half-Wave-Rectifier":
+        args = {}
+        args['list'] = circuitList
+        args['imgUrl'] = formData['imgUrl']
+        circuit, analysis, plot = half_wave_rectifier(formData['Vin'], formData['R'], formData['C'], formData['F'])
+        args['title'] = circuit.title
+        buf = BytesIO()
+        plot.savefig(buf, format="png")
+        data = base64.b64encode(buf.getbuffer()).decode("ascii")
+        args['plot'] = data
+        inputs = str('Inputs: Vin = {} V , R = {} Ohm , C = {} mF, F = {} Hz'.format(formData['Vin'], formData['R'], formData['C'],formData['F']))
+        args['inputs'] = inputs
+        return render_template("output.html", args=args)
+    
+    if formData['title'] == "Full-Wave-Rectifier":
+        args = {}
+        args['list'] = circuitList
+        args['imgUrl'] = formData['imgUrl']
+        circuit, analysis, plot = full_wave_rectifier(formData['Vin'], formData['R'], formData['C'], formData['F'])
+        args['title'] = circuit.title
+        buf = BytesIO()
+        plot.savefig(buf, format="png")
+        data = base64.b64encode(buf.getbuffer()).decode("ascii")
+        args['plot'] = data
+        inputs = str('Inputs: Vin = {} V , R = {} Ohm , C = {} mF, F = {} Hz'.format(formData['Vin'], formData['R'], formData['C'],formData['F']))
+        args['inputs'] = inputs
+        return render_template("output.html", args=args)
+
 
     return render_template("output.html", args=args)    
 
--- circuit.py
+++ circuit.py
@@ -1,13 +1,19 @@
 from app.circuits.OpAmps.OperationalAmplifier import BasicOperationalAmplifier
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import PySpice.Logging.Logging as Logging
 logger = Logging.setup_logging()
 from PySpice.Plot.BodeDiagram import bode_diagram
 from PySpice.Probe.Plot import plot
 from PySpice.Spice.Netlist import Circuit
 from PySpice.Unit import *
+from PySpice.Doc.ExampleTools import find_libraries
+from PySpice.Spice.Library import SpiceLibrary
+from PySpice.Physics.SemiConductor import ShockleyDiode
 
+libraries_path = find_libraries()
+spice_library = SpiceLibrary(libraries_path)
 
 # 1-voltage divider
 # 2- current divider
@@ -75,3 +81,155 @@ def current_divider(i,r1,r2):
         output[str(node)] = str(round(float(node),2)) + "A" 
 
     return circuit, analysis, output
+
+def diode_characteristic_curve(v,r):
+    circuit = Circuit('Diode Characteristic Curve')
+    circuit.include('./app/circuits/libraries/diode/switching/1N4148.lib')
+    circuit.V('input', 'in', circuit.gnd, u_V(float(v)))
+    circuit.R(1, 'in', 'out', u_Ohm(float(r))) # not required for simulation
+    circuit.X('D1', '1N4148', 'out', circuit.gnd)
+    #r# We simulate the circuit at these temperatures: 0, 25 and 100 °C.
+    # Fixme: Xyce ???
+    temperatures = [0, 25, 100]@u_Degree
+    analyses = {}
+    for temperature in temperatures:
+        simulator = circuit.simulator(temperature=temperature, nominal_temperature=temperature)
+        analysis = simulator.dc(Vinput=slice(-2, 5, .01))
+        analyses[float(temperature)] = analysis
+    silicon_forward_voltage_threshold = .7
+    shockley_diode = ShockleyDiode(Is=4e-9, degree=25)
+
+    def two_scales_tick_formatter(value, position):
+        if value >= 0:
+            return '{} mA'.format(value)
+        else:
+            return '{} nA'.format(value/100)
+    formatter = ticker.FuncFormatter(two_scales_tick_formatter)
+
+    figure, (ax1, ax2) = plt.subplots(2, figsize=(20, 10))
+    ax1.set_title('1N4148 Characteristic Curve ')
+    ax1.set_xlabel('Voltage [V]')
+    ax1.set_ylabel('Current')
+    ax1.grid()
+    ax1.set_xlim(-2, 2)
+    ax1.axvspan(-2, 0, facecolor='green', alpha=.2)
+    ax1.axvspan(0, silicon_forward_voltage_threshold, facecolor='blue', alpha=.1)
+    ax1.axvspan(silicon_forward_voltage_threshold, 2, facecolor='blue', alpha=.2)
+    ax1.set_ylim(-500, 750) # Fixme: round
+    ax1.yaxis.set_major_formatter(formatter)
+    Vd = analyses[25].out
+    # compute scale for reverse and forward region
+    forward_region = Vd >= 0@u_V
+    reverse_region = np.invert(forward_region)
+    scale =  reverse_region*1e11 + forward_region*1e3
+    #?# check temperature
+    for temperature in temperatures:
+        analysis = analyses[float(temperature)]
+        ax1.plot(Vd, - analysis.Vinput * scale)
+    ax1.plot(Vd, shockley_diode.I(Vd) * scale, 'black')
+    ax1.legend(['@ {} °C'.format(temperature)
+                for temperature in temperatures] + ['Shockley Diode Model Is = 4 nA'],
+            loc=(.02,.8))
+    ax1.axvline(x=0, color='black')
+    ax1.axhline(y=0, color='black')
+    ax1.axvline(x=silicon_forward_voltage_threshold, color='red')
+    ax1.text(-1, -100, 'Reverse Biased Region', ha='center', va='center')
+    ax1.text( 1, -100, 'Forward Biased Region', ha='center', va='center')
+    ax2.set_title('Resistance @ 25 °C')
+    ax2.grid()
+    ax2.set_xlim(-2, 3)
+    ax2.axvspan(-2, 0, facecolor='green', alpha=.2)
+    ax2.axvspan(0, silicon_forward_voltage_threshold, facecolor='blue', alpha=.1)
+    ax2.axvspan(silicon_forward_voltage_threshold, 3, facecolor='blue', alpha=.2)
+    analysis = analyses[25]
+    static_resistance = -analysis.out / analysis.Vinput
+    dynamic_resistance = np.diff(-analysis.out) / np.diff(analysis.Vinput)
+    ax2.semilogy(analysis.out, static_resistance, base=10)
+    ax2.semilogy(analysis.out[10:-1], dynamic_resistance[10:], base=10)
+    ax2.axvline(x=0, color='black')
+    ax2.axvline(x=silicon_forward_voltage_threshold, color='red')
+    ax2.axhline(y=1, color='red')
+    ax2.text(-1.5, 1.1, 'R limitation = 1 Ω', color='red')
+    ax2.legend(['{} Resistance'.format(x) for x in ('Static', 'Dynamic')], loc=(.05,.2))
+    ax2.set_xlabel('Voltage [V]')
+    ax2.set_ylabel('Resistance [Ω]')
+
+    return circuit, analysis, plt 
+
+def half_wave_rectifier(v,r,c,f):
+    circuit = Circuit('half-wave rectification')
+    circuit.include('./app/circuits/libraries/diode/switching/1N4148.lib')
+    source = circuit.SinusoidalVoltageSource('input', 'in', circuit.gnd, amplitude=u_V(float(v)), frequency=u_Hz(float(f)))
+    circuit.X('D1', '1N4148', 'in', 'output')
+    circuit.R('load', 'output', circuit.gnd, u_Ω(float(r)))
+
+    simulator = circuit.simulator(temperature=25, nominal_temperature=25)
+    analysis = simulator.transient(step_time=source.period/200, end_time=source.period*2)
+    figure, (ax1, ax2) = plt.subplots(2, figsize=(20, 10))
+
+    ax1.set_title('Half-Wave Rectification')
+    ax1.set_xlabel('Time [s]')
+    ax1.set_ylabel('Voltage [V]')
+    ax1.grid()
+    ax1.plot(analysis['in'])
+    ax1.plot(analysis.output)
+    ax1.legend(('input', 'output'), loc=(.05,.1))
+    ax1.set_ylim(float(-source.amplitude*1.1), float(source.amplitude*1.1))
+
+    circuit.C('1', 'output', circuit.gnd, u_mF(float(c)))
+
+    simulator = circuit.simulator(temperature=25, nominal_temperature=25)
+    analysis = simulator.transient(step_time=source.period/200, end_time=source.period*2)
+
+    ax2.set_title('Half-Wave Rectification with filtering')
+    ax2.set_xlabel('Time [s]')
+    ax2.set_ylabel('Voltage [V]')
+    ax2.grid()
+    ax2.plot(analysis['in'])
+    ax2.plot(analysis.output)
+    ax2.legend(('input', 'output'), loc=(.05,.1))
+    ax2.set_ylim(float(-source.amplitude*1.1), float(source.amplitude*1.1))
+    plt.tight_layout()
+
+    return circuit, analysis, plt
+     
+def full_wave_rectifier(v,r,c,f):
+    circuit = Circuit('Full-wave rectification')
+    circuit.include('./app/circuits/libraries/diode/switching/1N4148.lib')
+    source = circuit.SinusoidalVoltageSource('input', 'in', circuit.gnd, amplitude=u_V(float(v)), frequency=u_Hz(float(f)))
+    circuit.X('D1', '1N4148', 'in', 'output_plus')
+    circuit.R('load', 'output_plus', 'output_minus', u_Ω(float(r)))
+    circuit.X('D2', '1N4148', 'output_minus', circuit.gnd)
+    circuit.X('D3', '1N4148', circuit.gnd, 'output_plus')
+    circuit.X('D4', '1N4148', 'output_minus', 'in')
+
+    simulator = circuit.simulator(temperature=25, nominal_temperature=25)
+    analysis = simulator.transient(step_time=source.period/200, end_time=source.period*2)
+    figure, (ax3, ax4) = plt.subplots(2, figsize=(20, 10))
+
+    ax3.set_title('Full-Wave Rectification')
+    ax3.set_xlabel('Time [s]')
+    ax3.set_ylabel('Voltage [V]')
+    ax3.grid()
+    ax3.plot(analysis['in'])
+    ax3.plot(analysis.output_plus - analysis.output_minus)
+    ax3.legend(('input', 'output'), loc=(.05,.1))
+    ax3.set_ylim(float(-source.amplitude*1.1), float(source.amplitude*1.1))
+
+    circuit.C('1', 'output_plus', 'output_minus', u_mF(float(c)))
+
+    simulator = circuit.simulator(temperature=25, nominal_temperature=25)
+    analysis = simulator.transient(step_time=source.period/200, end_time=source.period*2)
+
+    ax4.set_title('Full-Wave Rectification with filtering')
+    ax4.set_xlabel('Time [s]')
+    ax4.set_ylabel('Voltage [V]')
+    ax4.grid()
+    ax4.plot(analysis['in'])
+    ax4.plot(analysis.output_plus - analysis.output_minus)
+    ax4.legend(('input', 'output'), loc=(.05,.1))
+    ax4.set_ylim(float(-source.amplitude*1.1), float(source.amplitude*1.1))
+
+    plt.tight_layout()
+
+    return circuit, analysis, plt
